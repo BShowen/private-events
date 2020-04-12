94177b203d52f4bc0a2166e4d818fb75bf07010f
Bradley Showen <bshowen@me.com>
2020-04-12 18:35:26 -0400

    remove index from EventManifest join table

--- db/migrate/20200327143839_create_event_manifests.rb
+++ db/migrate/20200327143839_create_event_manifests.rb
@@ -1,6 +1,6 @@
 class CreateEventManifests < ActiveRecord::Migration[6.0]
   def change
-    create_table :event_manifests do |t|
+    create_table :event_manifests, id: false do |t|
       t.references :attendee
       t.references :attended_event
     end
