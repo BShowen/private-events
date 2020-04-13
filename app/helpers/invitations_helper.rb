e24e6dee9a769c2ae99ed61baaa9d39c980a47bf
Bradley Showen <bshowen@me.com>
2020-04-13 08:29:45 -0400

    fix bug in InvitationsHelper

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,5 +1,6 @@
 module InvitationsHelper
     def current_user_has_invites
-        current_user.invites_received.any? {|invite| !invite.accepted? }
+        # current_user.invites_received.any? {|invite| !invite.accepted? }
+        current_user.invites_received.any?
     end
 end
